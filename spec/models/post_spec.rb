2b16bb8c903a66d853c1f106d1cb2f50656ea1fd
milen-ale <milenalebel2@gmail.com>
2022-11-26 17:18:06 +0300

    fix rubocop linter errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -28,35 +28,35 @@ RSpec.describe Post, type: :model do
       expect(@post.title).to(satisfy { |x| x.length <= 250 })
     end
 
-  it 'commentsCounter greater than or === 0' do
-    expect(@post.comments_counter).to be 0
-  end
+    it 'commentsCounter greater than or === 0' do
+      expect(@post.comments_counter).to be 0
+    end
 
-  it 'Should have 0 comments after creation' do
-    expect(@post.comments_counter).to eq 0
-  end
+    it 'Should have 0 comments after creation' do
+      expect(@post.comments_counter).to eq 0
+    end
 
-  it 'likesCounter greater than or === 0' do
-    @post.likes_counter = -1
-    expect(@post).to_not be_valid
-  end
+    it 'likesCounter greater than or === 0' do
+      @post.likes_counter = -1
+      expect(@post).to_not be_valid
+    end
 
-  it 'Should have 0 likes after creation' do
-    expect(@post.likes_counter).to eq 0
-  end
+    it 'Should have 0 likes after creation' do
+      expect(@post.likes_counter).to eq 0
+    end
 
-  it 'Should return n posts after creating n posts' do
-    Comment.create(post: @post, author: @user, text: 'Hi Tom!')
-    Comment.create(post: @post, author: @user, text: 'Hi Tom!')
-    expect(@post.comments_counter).to eq 2
-  end
+    it 'Should return n posts after creating n posts' do
+      Comment.create(post: @post, author: @user, text: 'Hi Tom!')
+      Comment.create(post: @post, author: @user, text: 'Hi Tom!')
+      expect(@post.comments_counter).to eq 2
+    end
 
-  it 'Should return 5 comments after creating more than 5 posts' do
-    Comment.create(post: @post, author: @user, text: 'Hi Tom!')
-    Comment.create(post: @post, author: @user, text: 'Hi Tom!')
-    Comment.create(post: @post, author: @user, text: 'Hi Tom!')
-    Comment.create(post: @post, author: @user, text: 'Hi Tom!')
-    expect(@post.return_recent_comments.length).to eq 4
+    it 'Should return 5 comments after creating more than 5 posts' do
+      Comment.create(post: @post, author: @user, text: 'Hi Tom!')
+      Comment.create(post: @post, author: @user, text: 'Hi Tom!')
+      Comment.create(post: @post, author: @user, text: 'Hi Tom!')
+      Comment.create(post: @post, author: @user, text: 'Hi Tom!')
+      expect(@post.return_recent_comments.length).to eq 4
+    end
   end
-end
 end
