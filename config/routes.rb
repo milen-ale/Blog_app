a0a0265acb0af61df2247ff72817f6b79253c581
milen-ale <milenalebel2@gmail.com>
2022-12-07 17:21:27 +0300

    setup devises

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   get 'likes/create'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new] do
