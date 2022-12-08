67e855e0a1ed99c96c9bb1c06f4626602af6a85f
MIlen Alebel <milenalebel2@gmail.com>
2022-12-08 17:19:42 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
- class User < ApplicationRecord
+  class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -13,4 +13,4 @@
   def return_recent_posts
     Post.limit(3).order(created_at: :desc)
   end
- end
+  end
