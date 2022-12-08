b6bdadae5c32e6b4b84e5639c205ec34a966631d
milen-ale <milenalebel2@gmail.com>
2022-12-08 12:14:25 +0300

    add divise sign up and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,27 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  get 'likes/create'
+  # get 'likes/create'
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
     end
+
+    
   end
+
+  
+ 
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#index"
+  # root "users#index"
 end
